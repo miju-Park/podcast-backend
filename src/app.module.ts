0189a2ea9828c24bad4b142898d19f7528435ca4
Miju Park <mijupark@Mijuui-MacBookPro.local>
2021-09-10 21:17:29 +0900

    feat: database connect URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,21 +21,25 @@ import { AuthModule } from "./auth/auth.module";
       ignoreEnvFile: process.env.NODE_ENV === "production",
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid("dev", "production", "test").required(),
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.string().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
-        DB_NAME: Joi.string().required(),
+        DB_HOST: Joi.string(),
+        DB_PORT: Joi.string(),
+        DB_USERNAME: Joi.string(),
+        DB_PASSWORD: Joi.string(),
+        DB_NAME: Joi.string(),
         PRIVATE_KEY: Joi.string().required(),
       }),
     }),
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      ...(process.env.DATABASE_URL
+        ? { url: process.env.DATABASE_URL }
+        : {
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+          }),
       synchronize: process.env.NODE_ENV !== "production",
       logging:
         process.env.NODE_ENV !== "production" &&

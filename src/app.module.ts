e74ff5e665a28e4cafe9fa02b625779c945f6bdc
Miju Park <mijupark@Mijuui-MacBookPro.local>
2021-09-10 20:58:15 +0900

    feat: add db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module, RequestMethod, MiddlewareConsumer } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
+import { ConfigModule } from "@nestjs/config";
 import { PodcastsModule } from "./podcast/podcasts.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Podcast } from "./podcast/entities/podcast.entity";
@@ -7,38 +8,58 @@ import { Episode } from "./podcast/entities/episode.entity";
 import { Review } from "./podcast/entities/review.entity";
 import { User } from "./users/entities/user.entity";
 import { UsersModule } from "./users/users.module";
+import * as Joi from "joi";
 import { JwtModule } from "./jwt/jwt.module";
 import { JwtMiddleware } from "./jwt/jwt.middleware";
 import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
+      ignoreEnvFile: process.env.NODE_ENV === "prod",
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid("dev", "prod", "test").required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        PRIVATE_KEY: Joi.string().required(),
+      }),
+    }),
     TypeOrmModule.forRoot({
-      type: "sqlite",
-      database: "db.sqlite3",
-      synchronize: true,
-      logging: process.env.NODE_ENV !== "test",
-      entities: [Podcast, Episode, User, Review]
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV !== "prod",
+      logging:
+        process.env.NODE_ENV !== "prod" && process.env.NODE_ENV !== "test",
+      entities: [Podcast, Episode, User, Review],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
       context: ({ req }) => {
         return { user: req["user"] };
-      }
+      },
     }),
     JwtModule.forRoot({
-      privateKey: "8mMJe5dMGORyoRPLvngA8U4aLTF3WasX"
+      privateKey: "8mMJe5dMGORyoRPLvngA8U4aLTF3WasX",
     }),
     PodcastsModule,
     UsersModule,
-    AuthModule
-  ]
+    AuthModule,
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(JwtMiddleware).forRoutes({
       path: "/graphql",
-      method: RequestMethod.POST
+      method: RequestMethod.POST,
     });
   }
 }
